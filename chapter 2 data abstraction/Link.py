8adb07649bfae001632d367720de3f6ec251ba11
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-05-18 10:29:35 +0630

    update

--- chapter 2 data abstraction/Link.py
+++ chapter 2 data abstraction/Link.py
@@ -14,3 +14,59 @@ class Link:
 
 	def __len__(self):
 		return 1 + len(self.rest)
+
+	def __repr__(self):
+		if self.rest :
+			rest_str = ',' + repr(self.rest)
+		else :
+			rest_str = ""
+		return 'Link({0}{1})'.format(self.first,rest_str)
+
+
+def extend_link(s,t):
+	if s is Link.empty:
+		return t
+	else :
+		return Link(s.first,extend_link(s.rest,t))
+
+
+
+def map_link(f,s):
+	if s is Link.empty :
+		return s
+	else :
+		return Link(f(s.first),map_link(f,s.rest))
+
+
+
+def filter_link(f,s):
+	if s is Link.empty :
+		return s
+	else :
+		filtered = filter_link(f,s.rest)
+		if f(s.first) :
+			return Link(s.first,filtered)
+		return filtered
+
+
+
+def join_link(s,seperator):
+	if s is Link.empty :
+		return ""
+
+	elif s.rest is Link.empty :
+		return str(s.first)
+
+	else :
+		return str(s.first) + seperator + join_link(s.rest,seperator)
+
+
+
+
+
+
+
+
+
+
+
