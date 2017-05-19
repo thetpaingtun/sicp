4a7acfeeb4ab944ee9b8b1d4b3c03b41fd18c093
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-05-19 22:19:08 +0630

    update

--- chapter 2 data abstraction/factor.py
+++ chapter 2 data abstraction/factor.py
@@ -1,3 +1,4 @@
+from math import sqrt
 
 def is_divisible(n,k):
 	return n % k == 0
@@ -7,7 +8,6 @@ def fact(n):
 	for k in range(1,n+1):
 		if is_divisible(n,k):
 			total += 1
-
 	return total
 
 
@@ -18,17 +18,20 @@ def count(f):
 	counted.count_call = 0
 	return counted
 
-#decorator
-def count(f):
-	def counted(*args):
-		counted.count_call += 1
-		return f(*args)
-	counted.count_call = 0
-	return counted
+
+def fact_fast(n):
+	sqrt_n = sqrt(n)
+	k , total = 1 , 0
+	while k < sqrt_n:
+		if is_divisible(n,k):
+			total += 2
+		k += 1
+	if k * k == n :
+		total += 1
+	return total
 
 
 
+ 
 
-counted_fact = count(fact)
-print(counted_fact(12))
-print("it took {} steps".format(counted_fact.count_call))
+
