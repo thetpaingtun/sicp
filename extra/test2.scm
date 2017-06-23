700d07096dd622c2f9c27e90ffaedec86432c049
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-06-23 20:42:54 +0630

    update

--- extra/test2.scm
+++ extra/test2.scm
@@ -1,2 +1,50 @@
 ;from chapter 15 to 
 
+
+(define (sort  lst)
+   ;selecton sort
+   (cond 
+      ((null? lst) lst)
+      ((= 1 (length lst)) lst)
+      (else 
+      (let 
+         (
+            (smallest (find-smallest lst))
+         )   
+         (cons smallest (sort (remove-once smallest lst)))
+      )
+      )
+    )   
+   
+)
+
+(define (find-smallest lst)
+   (cond 
+      ((null? (cdr lst)) (symbol->string (car lst)))
+      ((string<? (symbol->string (car lst)) (find-smallest (cdr lst))) (symbol->string (car lst)))
+      (else (find-smallest (cdr lst)))
+   )  
+)
+
+(define (remove-once s lst)
+   (cond 
+      ((null? lst) lst)  
+      ((string=? s (symbol->string (car lst))) (cdr lst))
+      (else (cons (car lst) (remove-once s (cdr lst))))
+   )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
